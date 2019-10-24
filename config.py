f6ea41bf85a0805d602f6ca34387f4f450285a0e
kayla <kaybarenz@gmail.com>
2019-10-24 14:04:22 -0400

    Kayla - fixed config

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 # encoding: utf-8
+import os
 
 SECRET_KEY = 'a unique and long key'
 TITLE = 'Riki'
 HISTORY_SHOW_MAX = 30
-PIC_BASE = '/static/content/'
-CONTENT_DIR = '/Users/kayba/PycharmProjects/Riki/content'
-USER_DIR = '/Users/kayba/PycharmProjects/Riki/user'
+PIC_BASE = os.getcwd() + '/static/content/'
+CONTENT_DIR = os.getcwd() + '/content'
+USER_DIR = os.getcwd() + '/user'
 NUMBER_OF_HISTORY = 5
 PRIVATE = True
